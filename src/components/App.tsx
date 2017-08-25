d6bcdd9c8a63a503a263bf35c635caf74013e67d
MPanahandeh <mpg.pan@gmail.com>
2017-08-25 09:41:20 -0400

    Addded an app component

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+export class App extends Component<undefined, undefined> {
+    constructor() {
+        super();
+    }
+    render() {
+        return (
+            <div>
+                <h1>App Component!</h1>
+                <p>Fill up ap here</p>
+            </div>
+        );
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/Hello";
+import { App } from "./components/App";
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+    // <Hello compiler="TypeScript" framework="React" />,
+    // document.getElementById("example")
+
+    <App />,
+    document.getElementById("app")
 );
